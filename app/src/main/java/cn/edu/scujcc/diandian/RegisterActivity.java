bf1d540a35d59494443b8e8a90d2196f46bff86f
Roger <2749234+rogeryan@users.noreply.github.com>
2020-05-09 17:21:37 +0800

    2班：为用户注册界面添加日历控件。

--- app/src/main/java/cn/edu/scujcc/diandian/RegisterActivity.java
+++ app/src/main/java/cn/edu/scujcc/diandian/RegisterActivity.java
@@ -3,12 +3,38 @@ package cn.edu.scujcc.diandian;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    private final static String TAG = "DianDian";
+    private TextInputLayout birthdayInput;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        birthdayInput = findViewById(R.id.r_birthday);
+
+        // new Builder()
+        MaterialDatePicker.Builder builder = MaterialDatePicker.Builder.datePicker();
+        //告诉builder我们想要的效果
+        builder.setTitleText(R.string.birthday_title);
+        MaterialDatePicker picker = builder.build();
+        //操作日历
+
+        //日历点击“确定”后的处理
+        picker.addOnPositiveButtonClickListener(s -> {
+            Log.d(TAG, "日历的结果是：" + s);
+            Log.d(TAG, "标题是：" + picker.getHeaderText());
+            birthdayInput.getEditText().setText(picker.getHeaderText());
+        });
+
+        birthdayInput.setEndIconOnClickListener(v -> {
+            //弹出日历选择框
+            Log.d(TAG, "生日图标被点击了！");
+            picker.show(getSupportFragmentManager(), picker.toString());
+        });
     }
 }
