6b3fd8edf568ea1d623c5f7c473707d2be8efac3
Roger <2749234+rogeryan@users.noreply.github.com>
2020-05-14 16:48:26 +0800

    2班：完成用户注册功能。

---
+++ app/src/main/java/cn/edu/scujcc/diandian/User.java
@@ -0,0 +1,95 @@
+package cn.edu.scujcc.diandian;
+
+import java.util.Date;
+
+/**
+ * 使用app的用户
+ */
+public class User {
+    private String id;
+    private String username;
+    private String password;
+    private String phone;
+    private String gender;
+    private Date birthday;
+    private Date lastLogin;
+    private String lastIp;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", phone='" + phone + '\'' +
+                ", gender='" + gender + '\'' +
+                ", birthday=" + birthday +
+                ", lastLogin=" + lastLogin +
+                ", lastIp='" + lastIp + '\'' +
+                '}';
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public Date getLastLogin() {
+        return lastLogin;
+    }
+
+    public void setLastLogin(Date lastLogin) {
+        this.lastLogin = lastLogin;
+    }
+
+    public String getLastIp() {
+        return lastIp;
+    }
+
+    public void setLastIp(String lastIp) {
+        this.lastIp = lastIp;
+    }
+}
--- app/src/main/java/cn/edu/scujcc/diandian/UserLab.java
+++ app/src/main/java/cn/edu/scujcc/diandian/UserLab.java
@@ -13,6 +13,8 @@ public class UserLab {
     public final static int USER_LOGIN_SUCCESS = 1;
     public final static int USER_LOGIN_FAIL = -1;
     public final static int USER_LOGIN_PASSWORD_ERROR = -2;
+    public final static int USER_REGISTER_FAIL = -3;
+    public final static int USER_REGISTER_SUCCESS = 2;
 
     private final static String TAG = "DianDian";
     private static UserLab INSTANCE = null;
@@ -58,6 +60,36 @@ public class UserLab {
                 Log.e(TAG, "登录失败", t);
                 Message msg = new Message();
                 msg.what = USER_LOGIN_FAIL;
+                handler.sendMessage(msg);
+            }
+        });
+    }
+
+    /**
+     * 向服务器发送用户注册的请求。
+     *
+     * @param user 用户填写的信息
+     */
+    public void register(User user, Handler handler) {
+        Retrofit retrofit = RetrofitClient.getInstance();
+        UserApi api = retrofit.create(UserApi.class);
+        Log.d(TAG, "准备注册的用户信息是：" + user);
+        Call<User> call = api.register(user);
+        call.enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                Log.d(TAG, "用户成功注册!");
+                Log.d(TAG, "返回数据是：" + response.body());
+                Message msg = new Message();
+                msg.what = USER_REGISTER_SUCCESS;
+                handler.sendMessage(msg);
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+                Log.e(TAG, "注册失败", t);
+                Message msg = new Message();
+                msg.what = USER_REGISTER_FAIL;
                 handler.sendMessage(msg);
             }
         });
